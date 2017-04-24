b787985a18975b2285ba05e256889c20e39e3e6a
rosie6965 <ryujihyeon6965@gmail.com>
2017-04-24 17:53:31 +0900

    success

--- app/src/main/java/com/rosie/accessibilityservice/Reflection.java
+++ app/src/main/java/com/rosie/accessibilityservice/Reflection.java
@@ -1,7 +1,11 @@
 package com.rosie.accessibilityservice;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.lang.reflect.Method;
 
 /**
@@ -16,26 +20,64 @@ public class Reflection {
     private final static String className = "android.view.SurfaceControl";
     private final static String methodName = "screenshot";
 
+    private static String STORE_DIRECTORY;
+    private static int IMAGES_PRODUCED = 0;
+
     Reflection(MyService service){
         this.service = service;
 
         try {
             mClass = Class.forName(className);
+            createDirectory();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
+
+    private void createDirectory (){
+
+        File externalFilesDir = service.getExternalFilesDir(null);
+        if (externalFilesDir != null) {
+            STORE_DIRECTORY = externalFilesDir.getAbsolutePath() + "/screenshots/";
+            File storeDirectory = new File(STORE_DIRECTORY);
+            if (!storeDirectory.exists()) {
+                boolean success = storeDirectory.mkdirs();
+                if (!success) {
+                    Log.e(TAG, "failed to create file storage directory.");
+                    return;
+                }
+            }
+        } else {
+            Log.e(TAG, "failed to create file storage directory, getExternalFilesDir is null.");
+            return;
+        }
+    }
+
+
     void printMethod()  {
         try{
 
-            Method screenshot = mClass.getMethod(methodName, Integer.class, Integer.class);
-            Object bitmap = screenshot.invoke(null, service.screenWidth, service.screenHeight);
+            Method[] methods = mClass.getMethods();
+           // Method screenshot = mClass.getMethod(methodName, Integer.class, Integer.class);
+            Method screenshot = methods[23];
+
+            Bitmap bitmap = (Bitmap) screenshot.invoke(null, service.screenWidth, service.screenHeight);
             Log.d(TAG, "Object created!");
+            saveFile(bitmap);
 
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+    void saveFile(Bitmap bitmap) throws FileNotFoundException {
 
+        FileOutputStream fos = null;
+        fos = new FileOutputStream(STORE_DIRECTORY + "/myscreen_" + IMAGES_PRODUCED + ".png");
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+
+
+        IMAGES_PRODUCED++;
+        Log.d(TAG, "captured image: " + IMAGES_PRODUCED);
 
     }
 
